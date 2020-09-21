67d1c1a2bc34af6bd766df7e855527f5beacd621
李贤元 <xianyuan_li@qq.com>
2020-09-21 18:26:13 +0800

    Gateway通过微服务名实现动态路由

--- cloud2020/cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/config/GateWayConfig.java
+++ cloud2020/cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/config/GateWayConfig.java
@@ -13,10 +13,24 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GateWayConfig {
 
+    /**
+     * 代码中注入RouteLocator的Bean的配置：
+     *  当访问地址http://localhost:9527/guonei时会自动转发到地址：http://news.baidu.com/guonei
+     * @param routeLocatorBuilder
+     * @return
+     */
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
         RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
         routes.route("path_route_atguitu",r->r.path("/guonei").uri("http://news.baidu.com/guonei")).build();
+        return routes.build();
+    }
+
+
+    @Bean
+    public RouteLocator customerRouteLocator2(RouteLocatorBuilder builder) {
+        RouteLocatorBuilder.Builder routes = builder.routes();
+        routes.route("path_route_atguigu2",r->r.path("/guoji").uri("http://news.baidu.com/guoji")).build();
         return routes.build();
     }
 
