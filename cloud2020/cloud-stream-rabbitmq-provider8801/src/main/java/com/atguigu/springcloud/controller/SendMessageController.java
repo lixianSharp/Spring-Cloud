c0f19a0fc1de011ce2caa60ca36e496915141225
李贤元 <xianyuan_li@qq.com>
2020-10-14 22:17:14 +0800

    Stream编码常用注解、消息驱动之生产者、消费者、重复消费、通过group解决消息重复消费、小时持久化

---
+++ cloud2020/cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/controller/SendMessageController.java
@@ -0,0 +1,24 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.service.IMessageProvider;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author lxy
+ * @Date 2020/9/14
+ * @Descript
+ **/
+@RestController
+public class SendMessageController {
+
+    @Resource
+    private IMessageProvider messageProvider;
+
+    @GetMapping(value = "/sendMessage")
+    public String sendMessage() {
+        return messageProvider.send();
+    }
+}
---
+++ cloud2020/cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/service/IMessageProvider.java
@@ -0,0 +1,10 @@
+package com.atguigu.springcloud.service;
+
+/**
+ * @author lxy
+ * @Date 2020/9/14
+ * @Descript
+ **/
+public interface IMessageProvider {
+    public String send();
+}
